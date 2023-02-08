2ead1f64e2ba95826f2b723176bf19d1d8f54e23
felipe.sampedro <pipesancho182@gmail.com>
2023-02-08 15:45:11 -0500

    desafio 17 aparentemente terminado

--- env.config.js
+++ env.config.js
@@ -1,7 +1,14 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
-  PORT: process.env.PORT || 8080,
-  DB_PASSWORD: process.env.DB_PASSWORD || '',
-  SESSION_SECRET: process.env.SESSION_SECRET || '',
+  NODE_ENV: process.env.NODE_ENV,
+  HOST: process.env.HOST,
+  PORT: process.env.PORT,
+  DATA_SOURCE: process.env.DATASOURCE, 
+  DB_NAME: process.env.DB_NAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,  
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  TEST_MAIL: process.env.TEST_MAIL,
+  PASSWORD_MAIL: process.env.PASSWORD_MAIL,
 }
--- routers/api/api.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authRoutes = require('./auth/auth.routes');
-
-const router = express.Router();
-
-//Routes
-router.use('/auth', authRoutes);
-
-module.exports = router;
